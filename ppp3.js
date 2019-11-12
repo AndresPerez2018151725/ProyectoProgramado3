b836a64d7f18a18caa48d9c7e7141f8aba5e4b2d
sergiomoya00 <43157614+sergiomoya00@users.noreply.github.com>
2019-11-12 16:18:41 -0600

    Método verPlantas

--- ppp3.js
+++ ppp3.js
@@ -10,25 +10,25 @@ var imagenOrnamental = getElementById("Insertar nombre de la imagen aca");
 // NO SE SI USAMOS ARRAYS O CON LINKED LISTS, COMO QUIERAN 
 
 //OBJETOS
-function Planta(codigo, nombre, alturaMax, precioVenta, imagen){
-    this.codigo=codigo;
-    this.nombre=nombre;
-    this.alturaMax=alturaMax;
-    this.precioVenta=precioVenta;
-    this.imagen=imagen;
+function Planta(codigo, nombre, alturaMax, precioVenta, imagen) {
+	this.codigo = codigo;
+	this.nombre = nombre;
+	this.alturaMax = alturaMax;
+	this.precioVenta = precioVenta;
+	this.imagen = imagen;
 }
 
 
-function PlantaOrnamental(codigo, nombre, alturaMax, precioVenta,imagen){
-    Planta.call(this,codigo);
-    Planta.call(this,nombre);
-    Planta.call(this,alturaMax);
-    Planta.call(this,precioVenta);
-    Planta.call(this,imagen);
+function PlantaOrnamental(codigo, nombre, alturaMax, precioVenta, imagen) {
+	Planta.call(this, codigo);
+	Planta.call(this, nombre);
+	Planta.call(this, alturaMax);
+	Planta.call(this, precioVenta);
+	Planta.call(this, imagen);
 }
 
-PlantaOrnamental.prototype = Object.create(Planta.prototype,{
-	constructor:{
+PlantaOrnamental.prototype = Object.create(Planta.prototype, {
+	constructor: {
 		configurable: true,
 		enumerable: true,
 		value: PlantaOrnamental,
@@ -37,23 +37,23 @@ PlantaOrnamental.prototype = Object.create(Planta.prototype,{
 });
 
 
-function registrarPlantaOrnamental(codigo, nombre, alturaMax, precioVenta,imagen){
-	var plantaOrnamental= new PlantaOrnamental(codigo, nombre, alturaMax, precioVenta,imagen);
+function registrarPlantaOrnamental(codigo, nombre, alturaMax, precioVenta, imagen) {
+	var plantaOrnamental = new PlantaOrnamental(codigo, nombre, alturaMax, precioVenta, imagen);
 	listaOrnamentales.push(plantaOrnamental);
 };
 
 
-function PlantaAromatica(codigo, nombre, alturaMax, precioVenta,imagen, propiedadesAromaticas) {
-    Planta.call(this,codigo);
-    Planta.call(this,nombre);
-    Planta.call(this,alturaMax);
-    Planta.call(this,precioVenta);
-    Planta.call(this,imagen);
-    this.propiedadesAromaticas = propiedadesAromaticas;
+function PlantaAromatica(codigo, nombre, alturaMax, precioVenta, imagen, propiedadesAromaticas) {
+	Planta.call(this, codigo);
+	Planta.call(this, nombre);
+	Planta.call(this, alturaMax);
+	Planta.call(this, precioVenta);
+	Planta.call(this, imagen);
+	this.propiedadesAromaticas = propiedadesAromaticas;
 }
 
-PlantaAromatica.prototype = Object.create(Planta.prototype,{
-	constructor:{
+PlantaAromatica.prototype = Object.create(Planta.prototype, {
+	constructor: {
 		configurable: true,
 		enumerable: true,
 		value: PlantaAromatica,
@@ -63,23 +63,23 @@ PlantaAromatica.prototype = Object.create(Planta.prototype,{
 
 ///FALTA AGARRAR LOS DATOS DEL HTML Y TAMBIÉN AGARRAR EN UN ARRAY LAS PROPIEDADES AROMÁTICAS
 
-function registrarPlantaAromatica(codigo, nombre, alturaMax, precioVenta,imagen, propiedadesAromaticas){
-	var plantaAromatica= new PlantaAromatica(codigo, nombre, alturaMax, precioVenta,imagen,propiedadesAromaticas);
+function registrarPlantaAromatica(codigo, nombre, alturaMax, precioVenta, imagen, propiedadesAromaticas) {
+	var plantaAromatica = new PlantaAromatica(codigo, nombre, alturaMax, precioVenta, imagen, propiedadesAromaticas);
 	listaAromaticas.push(plantaAromatica);
 };
 
 
-function PlantaMedicinal(codigo, nombre, alturaMax, precioVenta,imagen, propiedadesMedicinales) {
-    Planta.call(this,codigo);
-    Planta.call(this,nombre);
-    Planta.call(this,alturaMax);
-    Planta.call(this,precioVenta);
-    Planta.call(this,imagen);
-    this.propiedadesMedicinales = propiedadesMedicinales;
+function PlantaMedicinal(codigo, nombre, alturaMax, precioVenta, imagen, propiedadesMedicinales) {
+	Planta.call(this, codigo);
+	Planta.call(this, nombre);
+	Planta.call(this, alturaMax);
+	Planta.call(this, precioVenta);
+	Planta.call(this, imagen);
+	this.propiedadesMedicinales = propiedadesMedicinales;
 }
 
-PlantaMedicinal.prototype = Object.create(Planta.prototype,{
-	constructor:{
+PlantaMedicinal.prototype = Object.create(Planta.prototype, {
+	constructor: {
 		configurable: true,
 		enumerable: true,
 		value: PlantaMedicinal,
@@ -88,23 +88,23 @@ PlantaMedicinal.prototype = Object.create(Planta.prototype,{
 });
 
 ///FALTA AGARRAR LOS DATOS DEL HTML Y TAMBIÉN AGARRAR EN UN ARRAY LAS PROPIEDADES MEDICINALES
-function registrarPlantaMedicinal(codigo, nombre, alturaMax, precioVenta,imagen, propiedadesMedicinales){
-	var PlantaMedicinal= new PlantaMedicinal(codigo, nombre, alturaMax, precioVenta,imagen,propiedadesMedicinales);
+function registrarPlantaMedicinal(codigo, nombre, alturaMax, precioVenta, imagen, propiedadesMedicinales) {
+	var PlantaMedicinal = new PlantaMedicinal(codigo, nombre, alturaMax, precioVenta, imagen, propiedadesMedicinales);
 	listaMedicinales.push(PlantaMedicinal);
 };
 
 
-function ArbolFrutal(codigo, nombre, alturaMax, precioVenta,imagen, temporada) {
-    Planta.call(this,codigo);
-    Planta.call(this,nombre);
-    Planta.call(this,alturaMax);
-    Planta.call(this,precioVenta);
-    Planta.call(this,imagen);
-    this.temporada = temporada;
+function ArbolFrutal(codigo, nombre, alturaMax, precioVenta, imagen, temporada) {
+	Planta.call(this, codigo);
+	Planta.call(this, nombre);
+	Planta.call(this, alturaMax);
+	Planta.call(this, precioVenta);
+	Planta.call(this, imagen);
+	this.temporada = temporada;
 }
 
-ArbolFrutal.prototype = Object.create(Planta.prototype,{
-	constructor:{
+ArbolFrutal.prototype = Object.create(Planta.prototype, {
+	constructor: {
 		configurable: true,
 		enumerable: true,
 		value: ArbolFrutal,
@@ -113,8 +113,8 @@ ArbolFrutal.prototype = Object.create(Planta.prototype,{
 });
 
 ///FALTA AGARRAR LOS DATOS DEL HTML Y TAMBIÉN AGARRAR EN UN ARRAY LAS TEMPORADAS
-function registrarArbolFrutal(codigo, nombre, alturaMax, precioVenta,imagen, temporada){
-	var arbolFrutal= new ArbolFrutal(codigo, nombre, alturaMax, precioVenta,imagen, temporada);
+function registrarArbolFrutal(codigo, nombre, alturaMax, precioVenta, imagen, temporada) {
+	var arbolFrutal = new ArbolFrutal(codigo, nombre, alturaMax, precioVenta, imagen, temporada);
 	listaArboles.push(arbolFrutal);
 };
 
@@ -122,123 +122,178 @@ function registrarArbolFrutal(codigo, nombre, alturaMax, precioVenta,imagen, tem
 
 //LINKED LIST, SE PUEDE USAR O NO 
 function LinkedList() {
-    this.head = null;
-    this.tail = null;
+	this.head = null;
+	this.tail = null;
 }
 function NodeLinked(value, next, prev) {
-    this.value = value;
-    this.next = next;
-    this.prev = prev;
+	this.value = value;
+	this.next = next;
+	this.prev = prev;
 }
 
 
-LinkedList.prototype.InsertHead = function(value){
-    const newNodo = new NodeLinked(value, this.head, null);
-    if (this.head) this.head.prev = newNodo;
-    else this.tail = newNodo;
-    this.head = newNodo;
+LinkedList.prototype.InsertHead = function (value) {
+	const newNodo = new NodeLinked(value, this.head, null);
+	if (this.head) this.head.prev = newNodo;
+	else this.tail = newNodo;
+	this.head = newNodo;
 };
 
 
 LinkedList.prototype.InsertTail = function (vaule) {
-    const newNodo = new NodeLinked(value, null, this.tail);
-    if (this.tail) this.tail.next = newNodo;
-    else this.head = newNodo;
-    this.tail = newNodo;
+	const newNodo = new NodeLinked(value, null, this.tail);
+	if (this.tail) this.tail.next = newNodo;
+	else this.head = newNodo;
+	this.tail = newNodo;
 }
 
 LinkedList.prototype.search = function (buscar) {
-    let current = this.head;
-    while (current) {
-        if (current.value === buscar) return current;
-        current = current.next;
-    }
-    return null;
+	let current = this.head;
+	while (current) {
+		if (current.value === buscar) return current;
+		current = current.next;
+	}
+	return null;
 }
 
-function buscarPorNombre(nombre){
+function buscarPorNombre(nombre) {
 	var resultado = new Array();
-	for (i=0;i<listaArboles.length;i++){
-		if (listaArboles[i].nombre==nombre){
+	for (i = 0; i < listaArboles.length; i++) {
+		if (listaArboles[i].nombre == nombre) {
 			resultado.push(listaArboles[i]);
 			break;
 		}
-    }
-    for (i=0;i<listaAromaticas.length;i++){
-		if (listaAromaticas[i].nombre==nombre){
+	}
+	for (i = 0; i < listaAromaticas.length; i++) {
+		if (listaAromaticas[i].nombre == nombre) {
 			resultado.push(listaAromaticas[i]);
 			break;
 		}
-    }
-    for (i=0;i<listaMedicinales.length;i++){
-		if (listaMedicinales[i].nombre==nombre){
+	}
+	for (i = 0; i < listaMedicinales.length; i++) {
+		if (listaMedicinales[i].nombre == nombre) {
 			resultado.push(listaMedicinales[i]);
 			break;
 		}
-    }
-    for (i=0;i<listaOrnamentales.length;i++){
-		if (listaOrnamentales[i].nombre==nombre){
+	}
+	for (i = 0; i < listaOrnamentales.length; i++) {
+		if (listaOrnamentales[i].nombre == nombre) {
 			resultado.push(listaOrnamentales[i]);
 			break;
 		}
-    }
+	}
 	return resultado;
 };
 
-function buscarPorTemporada(temporada){
+function buscarPorTemporada(temporada) {
 	var resultado = new Array();
-	for (i=0;i<listaArboles.length;i++){
-		if (listaArboles[i].temporada==temporada){
+	for (i = 0; i < listaArboles.length; i++) {
+		if (listaArboles[i].temporada == temporada) {
 			resultado.push(listaArboles[i]);
 			break;
 		}
-    }
-    return resultado;
+	}
+	return resultado;
 };
 
-function buscarPorPrecio(precio){
+function buscarPorPrecio(precio) {
 	var resultado = new Array();
-	for (i=0;i<listaArboles.length;i++){
-		if (listaArboles[i].precioVenta==precio){
+	for (i = 0; i < listaArboles.length; i++) {
+		if (listaArboles[i].precioVenta == precio) {
 			resultado.push(listaArboles[i]);
 			break;
 		}
-    }
-    for (i=0;i<listaAromaticas.length;i++){
-		if (listaAromaticas[i].precioVenta==precio){
+	}
+	for (i = 0; i < listaAromaticas.length; i++) {
+		if (listaAromaticas[i].precioVenta == precio) {
 			resultado.push(listaAromaticas[i]);
 			break;
 		}
-    }
-    for (i=0;i<listaMedicinales.length;i++){
-		if (listaMedicinales[i].precioVenta==precio){
+	}
+	for (i = 0; i < listaMedicinales.length; i++) {
+		if (listaMedicinales[i].precioVenta == precio) {
 			resultado.push(listaMedicinales[i]);
 			break;
 		}
-    }
-    for (i=0;i<listaOrnamentales.length;i++){
-		if (listaOrnamentales[i].precioVenta==precio){
+	}
+	for (i = 0; i < listaOrnamentales.length; i++) {
+		if (listaOrnamentales[i].precioVenta == precio) {
 			resultado.push(listaOrnamentales[i]);
 			break;
 		}
-    }
+	}
 	return resultado;
 };
 
-function buscarPorPropiedades(propiedades){
+function buscarPorPropiedades(propiedades) {
 	var resultado = new Array();
-    for (i=0;i<listaAromaticas.length;i++){
-		if (listaAromaticas[i].propiedadesAromaticas==propiedades){
+	for (i = 0; i < listaAromaticas.length; i++) {
+		if (listaAromaticas[i].propiedadesAromaticas == propiedades) {
 			resultado.push(listaAromaticas[i]);
 			break;
 		}
-    }
-    for (i=0;i<listaMedicinales.length;i++){
-		if (listaMedicinales[i].propiedadesMedicinales==propiedades){
+	}
+	for (i = 0; i < listaMedicinales.length; i++) {
+		if (listaMedicinales[i].propiedadesMedicinales == propiedades) {
 			resultado.push(listaMedicinales[i]);
 			break;
 		}
-    }
-    return resultado;
+	}
+	return resultado;
 };
 
+function verPlantas() {
+	var resultado = new Array();
+	for (i = 0; i < listaArboles.length; i++) {
+		resultado.push(listaArboles[i].nombre);
+		resultado.push(listaArboles[i].imagen);
+	}
+	for (i = 0; i < listaAromaticas.length; i++) {
+		resultado.push(listaAromaticas[i].nombre);
+		resultado.push(listaAromaticas[i].imagen);
+	}
+	for (i = 0; i < listaMedicinales.length; i++) {
+		resultado.push(listaMedicinales[i].nombre);
+		resultado.push(listaMedicinales[i].imagen);
+	}
+	for (i = 0; i < listaOrnamentales.length; i++) {
+		resultado.push(listaOrnamentales[i].nombre);
+		resultado.push(listaOrnamentales[i].imagen);
+	}
+	return resultado;
+};
+
+function verPlantasNombres() {
+	var resultado = new Array();
+	for (i = 0; i < listaArboles.length; i++) {
+		resultado.push(listaArboles[i].nombre);
+	}
+	for (i = 0; i < listaAromaticas.length; i++) {
+		resultado.push(listaAromaticas[i].nombre);
+	}
+	for (i = 0; i < listaMedicinales.length; i++) {
+		resultado.push(listaMedicinales[i].nombre);
+	}
+	for (i = 0; i < listaOrnamentales.length; i++) {
+		resultado.push(listaOrnamentales[i].nombre);
+	}
+	return resultado;
+
+};
+
+function verPlantasImagen() {
+	var resultado = new Array();
+	for (i = 0; i < listaArboles.length; i++) {
+		resultado.push(listaArboles[i].imagen);
+	}
+	for (i = 0; i < listaAromaticas.length; i++) {
+		resultado.push(listaAromaticas[i].imagen);
+	}
+	for (i = 0; i < listaMedicinales.length; i++) {
+		resultado.push(listaMedicinales[i].imagen);
+	}
+	for (i = 0; i < listaOrnamentales.length; i++) {
+		resultado.push(listaOrnamentales[i].imagen);
+	}
+	return resultado;
+};
